6e9f05972019ff73bb3d03176d65109fd291e39a
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-08-25 23:18:15 -0400

    Add fn close comments

--- genesis-cta-widget.php
+++ genesis-cta-widget.php
@@ -21,13 +21,15 @@ class Genesis_CTA_Widget extends WP_Widget {
 
 
     function Genesis_CTA_Widget() {
+        
 		// Instantiate the parent object
 		parent::__construct(
 	            'genesis_cta_widget', // Base ID
         	    __('Genesis Call To Action Widget', 'text_domain'), // Name
  	           array( 'description' => __( 'Call To Action Widget with text, background image, alignment, and button icon support for the Genesis Framework.', 'text_domain' ), ) // Args
 		);
-	}
+	
+    } // Genesis_CTA_Widget()
     
     
 	function widget( $args, $instance ) {
@@ -43,7 +45,8 @@ class Genesis_CTA_Widget extends WP_Widget {
         echo '</div>';
         
 		echo $args['after_widget'];
-	}
+	
+    } // widget()
     
 
 } // class Genesis_CTA_Widget
