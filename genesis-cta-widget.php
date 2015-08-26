91fb0fde4d5be05db673a398abb9174e6f457f39
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-08-25 21:48:29 -0400

    Init main plugin file

---
+++ genesis-cta-widget.php
@@ -0,0 +1,9 @@
+<?php 
+
+/*
+Plugin Name: Genesis Call To Action Widget
+Description: A Genesis Framework Call To Action Widget with text, background image, alignment, and button icon support.
+Version: 1.0
+Author: Jordan Pakrosnis
+Author URI: http://JordanPak.com/
+*/
